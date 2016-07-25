33bb35e4d4439a3a4abe9780f398651457fe952d
Mike <IRDeNial@users.noreply.github.com>
2016-07-25 04:32:46 -0700

    Don't pollute global namespace
    Added a custom namespace to avoid polluting the global namespace.
    It bugs me...

--- main.js
+++ main.js
@@ -1,17 +1,34 @@
-(function(){
-    var workArea = document.querySelector('.header-toolbar');
-    var newToolbarItem = document.createElement('div');
-    newToolbarItem.style.marginLeft = '8px';
-    newToolbarItem.innerHTML = '<button class="sloppy-show-channels-button active" style="" type="button"><span style="display: block; color: #fefefe; -webkit-transform: scale(2.0, 1.0); -moz-transform:scale(2.0, 1.0); -ms-transform:scale(2.0, 1.0); -o-transform:scale(2.0, 1.0); transform: scale(6,1); width: 0; height: 5px; text-align: center; position: absolute; top: 1px; left: 0;">-</span><span style="display: block; color: #fefefe; -webkit-transform: scale(2.0, 1.0); -moz-transform:scale(2.0, 1.0); -ms-transform:scale(2.0, 1.0); -o-transform:scale(2.0, 1.0); transform: scale(6,1); height: 5px; position: absolute; top: 6px; left: 0;">-</span><span style="display: block; color: #fefefe; -webkit-transform: scale(2.0, 1.0); -moz-transform:scale(2.0, 1.0); -ms-transform:scale(2.0, 1.0); -o-transform:scale(2.0, 1.0); transform: scale(6,1); width: 100%; height: 5px; position: absolute; top: 11px; left: 0;">-</span></button>';
-    workArea.insertBefore(newToolbarItem,document.querySelector('.header-toolbar div.separator'));
+(function(){   
+    var ChannelMinimizeAddon = {
+        addToolbarItem: function() {
+            if(document.querySelectorAll('.show-channels-button').length == 0) {
+                var workArea = document.querySelector('.header-toolbar');
+                var newToolbarItem = document.createElement('div');
+                newToolbarItem.style.marginLeft = '8px';
+                newToolbarItem.innerHTML = '<button class="show-channels-button active" style="" type="button"><span style="display: block; color: #fefefe; -webkit-transform: scale(2.0, 1.0); -moz-transform:scale(2.0, 1.0); -ms-transform:scale(2.0, 1.0); -o-transform:scale(2.0, 1.0); transform: scale(6,1); width: 0; height: 5px; text-align: center; position: absolute; top: 1px; left: 0;">-</span><span style="display: block; color: #fefefe; -webkit-transform: scale(2.0, 1.0); -moz-transform:scale(2.0, 1.0); -ms-transform:scale(2.0, 1.0); -o-transform:scale(2.0, 1.0); transform: scale(6,1); height: 5px; position: absolute; top: 6px; left: 0;">-</span><span style="display: block; color: #fefefe; -webkit-transform: scale(2.0, 1.0); -moz-transform:scale(2.0, 1.0); -ms-transform:scale(2.0, 1.0); -o-transform:scale(2.0, 1.0); transform: scale(6,1); width: 100%; height: 5px; position: absolute; top: 11px; left: 0;">-</span></button>';
+                workArea.insertBefore(newToolbarItem,document.querySelector('.header-toolbar div.separator'));
 
-    document.querySelector('.sloppy-show-channels-button').addEventListener('click',function(e){
-        if(this.classList.contains('active')) {
-            document.querySelector('.channels-wrap').style.display = 'none';
-            this.classList.toggle('active');
-        } else {
-            document.querySelector('.channels-wrap').style.display = 'flex';
-            this.classList.toggle('active');
+                document.querySelector('.show-channels-button').addEventListener('click',function(e){
+                    if(this.classList.contains('active')) {
+                        document.querySelector('.channels-wrap').style.display = 'none';
+                        this.classList.toggle('active');
+                    } else {
+                        document.querySelector('.channels-wrap').style.display = 'flex';
+                        this.classList.toggle('active');
+                    }
+                });
+            }
         }
-    });
+    };
+
+    var guilds = document.querySelectorAll('.guild');
+    for(var i = 0;i < guilds.length; ++i) {
+        guilds[i].addEventListener('click',function(){
+            window.setTimeout(function(){
+                ChannelMinimizeAddon.addToolbarItem();
+            },1);
+        });
+    }
+
+    ChannelMinimizeAddon.addToolbarItem();
 })();
