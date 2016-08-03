149aa08332657506c63344a72d2400f38b6e0313
Mike Orozco <mike.orozco94@gmail.com>
2016-08-02 23:54:45 -0700

    Multiple Changes
    * Proper use of prototype
    * Use of observer to check when it's okay to manipulate DOM.
    * Link author name to profile
    * Ensure no more than one collapse button exists at a time.

--- channelcollapsebutton.plugin.js
+++ channelcollapsebutton.plugin.js
@@ -1,27 +1,5 @@
 //META{"name":"channelCollapseButton"}*//
-function channelCollapseButton(){};
-
-channelCollapseButton.prototype.addToolbarItem = function(){
-    if(document.querySelectorAll('.show-channels-button').length == 0) {
-        var newToolbarItem = document.createElement('button');
-        newToolbarItem.type = 'button';
-        newToolbarItem.classList.add('show-channels-button');
-        newToolbarItem.classList.add('active');
-        newToolbarItem.innerHTML = '<span style="display: block; color: #fefefe; -webkit-transform: scale(2.0, 1.0); -moz-transform:scale(2.0, 1.0); -ms-transform:scale(2.0, 1.0); -o-transform:scale(2.0, 1.0); transform: scale(6,1); width: 0; height: 5px; text-align: center; position: absolute; top: 1px; left: 0;">-</span><span style="display: block; color: #fefefe; -webkit-transform: scale(2.0, 1.0); -moz-transform:scale(2.0, 1.0); -ms-transform:scale(2.0, 1.0); -o-transform:scale(2.0, 1.0); transform: scale(6,1); width: 0; height: 5px; position: absolute; top: 6px; left: 0;">-</span><span style="display: block; color: #fefefe; -webkit-transform: scale(2.0, 1.0); -moz-transform:scale(2.0, 1.0); -ms-transform:scale(2.0, 1.0); -o-transform:scale(2.0, 1.0); transform: scale(6,1); width: 0; height: 5px; position: absolute; top: 11px; left: 0;">-</span>';
-
-        document.querySelector('.header-toolbar').insertBefore(newToolbarItem,document.querySelector('.header-toolbar div.separator'));
-        
-        document.querySelector('.show-channels-button').addEventListener('click',function(e){
-            if(this.classList.contains('active')) {
-                document.querySelector('.channels-wrap').style.display = 'none';
-                this.classList.toggle('active');
-            } else {
-                document.querySelector('.channels-wrap').style.display = 'flex';
-                this.classList.toggle('active');
-            }
-        });
-    }
-}
+var  channelCollapseButton = function(){};
 
 channelCollapseButton.prototype.getName = function(){
     return "Channel Collapse Button";
@@ -30,26 +8,65 @@ channelCollapseButton.prototype.getDescription = function(){
     return "Create a button that allows for hiding of channel list";
 };
 channelCollapseButton.prototype.getVersion = function(){
-    return "1.0.0";
+    return "1.0.1";
 };
 channelCollapseButton.prototype.getAuthor = function(){
-    return "DeNial";
+    return '<a href="https://github.com/IRDeNial" target="_BLANK">DeNial</a>';
 };
-channelCollapseButton.prototype.start = function(){
-    this.addToolbarItem();
-	console.log("channelCollapseButton loaded");
+
+channelCollapseButton.prototype.load = function(){
+    this.loaded = false;
+    this.bdIsLoaded = false;
+    this.collapseButtonDisplayed = false;
+
+    this.addToolbarItem = function(){
+        if(!this.collapseButtonDisplayed) {
+            var newToolbarItem = document.createElement('button');
+            newToolbarItem.type = 'button';
+            newToolbarItem.classList.add('show-channels-button');
+            newToolbarItem.classList.add('active');
+            newToolbarItem.innerHTML = '<span style="display: block; color: #fefefe; -webkit-transform: scale(2.0, 1.0); -moz-transform:scale(2.0, 1.0); -ms-transform:scale(2.0, 1.0); -o-transform:scale(2.0, 1.0); transform: scale(6,1); width: 0; height: 5px; text-align: center; position: absolute; top: 1px; left: 0;">-</span><span style="display: block; color: #fefefe; -webkit-transform: scale(2.0, 1.0); -moz-transform:scale(2.0, 1.0); -ms-transform:scale(2.0, 1.0); -o-transform:scale(2.0, 1.0); transform: scale(6,1); width: 0; height: 5px; position: absolute; top: 6px; left: 0;">-</span><span style="display: block; color: #fefefe; -webkit-transform: scale(2.0, 1.0); -moz-transform:scale(2.0, 1.0); -ms-transform:scale(2.0, 1.0); -o-transform:scale(2.0, 1.0); transform: scale(6,1); width: 0; height: 5px; position: absolute; top: 11px; left: 0;">-</span>';
+
+            document.querySelector('.header-toolbar').insertBefore(newToolbarItem,document.querySelector('.header-toolbar div.separator'));
+            
+            document.querySelector('.show-channels-button').addEventListener('click',function(e){
+                if(this.classList.contains('active')) {
+                    document.querySelector('.channels-wrap').style.display = 'none';
+                    this.classList.toggle('active');
+                } else {
+                    document.querySelector('.channels-wrap').style.display = 'flex';
+                    this.classList.toggle('active');
+                }
+            });
+            this.collapseButtonDisplayed = true;
+        }
+    }
 };
+channelCollapseButton.prototype.unload = function(){};
 channelCollapseButton.prototype.stop = function(){
     if(document.querySelectorAll('.show-channels-button').length) {
         document.querySelector('.show-channels-button').outerHTML = "";
         delete document.querySelector('.show-channels-button');
     }
     document.querySelector('.channels-wrap').style.display = 'flex';
-    console.log("channelCollapseButton unloaded");
-};
-channelCollapseButton.prototype.load = function(){
-    this.addToolbarItem();
+    this.loaded = false;
 };
 channelCollapseButton.prototype.onSwitch = function(){
-    this.addToolbarItem();
+    if(this.loaded) {
+        this.addToolbarItem();
+    }
 }
+channelCollapseButton.prototype.observer = function(e) {
+    if(!this.loaded) {
+        if(e.target.classList.contains("guilds")) {
+            this.loaded = true;
+            this.bdIsLoaded = true;
+            this.addToolbarItem();
+        }
+    }
+};
+channelCollapseButton.prototype.start = function(){
+    if(this.bdIsLoaded) {
+        this.addToolbarItem();
+    }
+};
