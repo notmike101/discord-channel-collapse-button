5e0314f45728f5f7d33533029ee37df1a0da425c
ThatGeekyWeeb <ssfgames13@gmail.com>
2020-07-31 03:27:05 -0400

    Update for latest version  html is now working!   using variables now!    remove "width" : other bottons now work too!     (They did not before)

--- main.js
+++ main.js
@@ -5,7 +5,7 @@
                 var newToolbarItem = document.createElement('span');
                 newToolbarItem.classList.add('show-channels-button');
                 newToolbarItem.classList.add('active');
-                newToolbarItem.classList.add(document.querySelector('[class*="headerBar"] span[class^="iconMargin"]').classList[0])
+                newToolbarItem.classList.add(document.querySelector('[class*="toolbar"]').classList[0])
                 newToolbarItem.style.width = '26px'
                 newToolbarItem.style.position = 'relative'
                 newToolbarItem.style.top = '-2px'
@@ -13,16 +13,16 @@
                 newToolbarItem.style.flexDirection = 'column'
                 newToolbarItem.style.justifyContent = 'flex-start'
                 newToolbarItem.style.cursor = 'pointer'
-                newToolbarItem.innerHTML = '<span style="display: block; color: #fefefe; -webkit-transform: scale(2.0, 1.0); -moz-transform:scale(2.0, 1.0); -ms-transform:scale(2.0, 1.0); -o-transform:scale(2.0, 1.0); transform: scale(5,1); width: 0; height: 5px; text-align: center;">-</span><span style="display: block; color: #fefefe; -webkit-transform: scale(5.0, 1.0); -moz-transform:scale(2.0, 1.0); -ms-transform:scale(2.0, 1.0); -o-transform:scale(2.0, 1.0); transform: scale(5,1); width: 0; height: 5px;">-</span><span style="display: block; color: #fefefe; -webkit-transform: scale(2.0, 1.0); -moz-transform:scale(2.0, 1.0); -ms-transform:scale(2.0, 1.0); -o-transform:scale(2.0, 1.0); transform: scale(5,1); width: 0; height: 5px;">-</span>';
+                newToolbarItem.innerHTML = '<span class="show-channels-button toolbar-1t6TWx active" style="width: 26px; position: relative; top: -2px; display: flex; flex-direction: column; justify-content: flex-start; cursor: pointer;"><span class="show-channels-button toolbar-1t6TWx" style="width: 26px;position: relative;top: -6px;display: flex;flex-direction: column;justify-content: flex-start;cursor: pointer;font-family: serif !important;"><span class="show-channels-button toolbar-1t6TWx active" style="position: relative;top: -2px;display: flex;flex-direction: column;justify-content: flex-start;cursor: pointer;padding-right: 30px;padding-bottom: 25px;"><span class="show-channels-button toolbar-1t6TWx active" style="position: initial;display: flex;flex-direction: column;justify-content: flex-start;cursor: pointer;"><span class="show-channels-button toolbar-1t6TWx active" style="position: absolute;display: flex;flex-direction: column;justify-content: flex-start;cursor: pointer;margin-bottom: 15px;"><span style="display: block;color: var(--text-normal);-webkit-transform: scale(2.0, 1.0);-moz-transform:scale(2.0, 1.0);-ms-transform:scale(2.0, 1.0);-o-transform:scale(2.0, 1.0);transform: scale(5,2);width: 0;height: 1px;text-align: center;">-</span><span style="display: block; color: var(--text-normal); -webkit-transform: scale(5.0, 1.0);-moz-transform:scale(2.0, 1.0);-ms-transform:scale(2.0, 1.0);-o-transform:scale(2.0, 1.0);transform: scale(5,2);width: 0;height: 19px;">-</span><span style="display: block; color: var(--text-normal); -webkit-transform: scale(2.0, 1.0);-moz-transform:scale(2.0, 1.0);-ms-transform:scale(2.0, 1.0);-o-transform:scale(2.0, 1.0);transform: scale(5,2);width: 0;padding-bottom: 5px;">-</span>';
 
-                document.querySelector('[class*="headerBar"]>:nth-child(2)').insertBefore(newToolbarItem,document.querySelector('[class^="divider"]'));
+                document.querySelector('[class*="toolbar"]').insertBefore(newToolbarItem,document.querySelector('.search-36MZv-'));
 
                 document.querySelector('.show-channels-button').addEventListener('click',function(e){
                     if(this.classList.contains('active')) {
-                        document.querySelector('[class^="channels"]').style.display = 'none';
+                        document.querySelector('.sidebar-2K8pFh').style.display = 'none';
                         this.classList.toggle('active');
                     } else {
-                        document.querySelector('[class^="channels"]').style.display = 'flex';
+                        document.querySelector('.sidebar-2K8pFh').style.display = 'flex';
                         this.classList.toggle('active');
                     }
                 });
